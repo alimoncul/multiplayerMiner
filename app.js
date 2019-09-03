8e8dbe517f300bd72f7c6f66751bf39fcfafea8a
Alim Öncül <alimoncul@gmail.com>
2019-09-03 12:09:00 +0300

    Rendering & player images

--- app.js
+++ app.js
@@ -9,17 +9,36 @@ app.get("/", function(req, res) {
 app.use("/client", express.static(__dirname + "/client"));
 AppServer.listen(2000);
 
+var gameInstance = {
+    map: generateMap()
+};
+
+function updateMap(currentMap) {
+    tempArr = [];
+    for (let i = 0; i < currentMap.length; i++) {
+        tempArr.push(currentMap[i]);
+    }
+    while (tempArr.filter(x => x == 1).length <= 20) {
+        tempArr[Math.floor(Math.random() * 400)] = 1;
+    }
+    return currentMap;
+}
+
 function generateMap() {
     let gameMap = [];
+    let arrRet = [];
+    let range = 0;
     for (let i = 0; i < 400; i++) {
         gameMap.push(0);
     }
-    console.log(gameMap);
-    while (gameMap.filter(x => x == 1).length < 20) {
+    while (gameMap.filter(x => x == 1).length <= 20) {
         gameMap[Math.floor(Math.random() * 400)] = 1;
     }
-    console.log(gameMap);
-    return gameMap;
+    for (let b = 0; b < 20; b++) {
+        arrRet.push(gameMap.slice(range, range + 20));
+        range += 20;
+    }
+    return arrRet;
 }
 var playerList = {};
 var socketList = {};
@@ -28,12 +47,20 @@ var Player = function(id) {
         x: 400,
         y: 400,
         id: id,
+        points: 0,
         number: "" + Math.floor(10 * Math.random()),
         right: false,
         left: false,
         up: false,
         down: false,
-        speed: 10
+        speed: 5
+    };
+    self.mining = function(CurrentMap) {
+        if (CurrentMap[Math.floor(self.x / 40)][Math.floor(self.y / 40)] == 1) {
+            self.points += 1;
+            CurrentMap[self.x / 40][self.y / 40] = 0;
+            let updatedMap = updateMap(CurrentMap);
+        }
     };
     self.movement = function() {
         if (self.right) {
@@ -65,10 +92,6 @@ io.sockets.on("connection", function(socket) {
         delete playerList[socket.id];
     });
 
-    socket.on("mapUpdate", function() {
-        var gameMap = generateMap();
-    });
-
     socket.on("keyPressed", function(data) {
         if (data.inputId === "up") {
             player.up = data.state;
@@ -92,5 +115,6 @@ setInterval(function() {
     for (var i in socketList) {
         var socket = socketList[i];
         socket.emit("newPositions", package);
+        socket.emit("map", gameInstance.map);
     }
 }, 1000 / 25);
